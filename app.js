8695896214040f9ff11c958ec447fc63388a2848
ilyasgaripov97 <ilyasgaripov97@gmail.com>
2021-03-23 13:40:52 +0300

    Add functions to store/check user for authentication

--- app.js
+++ app.js
@@ -9,44 +9,43 @@ const pool = require('./db/db').pool;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const storeUser = (username, passwordHash) => {
-  // Формируем данные для запроса к базе
-  const query = {
-    text: "INSERT INTO a_user(username, password_hash) VALUES ($1, $2)",
-    values: [username , passwordHash]
-  }
 
-  // Добавляем пользователя в базу, @TODO добавить уникальный индекс
-  // для того чтобы не было двух одинаковых пользователей
-  pool
-    .query(query, (err, res) => {
-      if (err) {
-        console.log(err.stack);
-      } else {
-        console.log(res.rows[0])
-      }
-    })  
+const storeUser = async (username, passwordHash) => {
+  try {
+    query = {
+      text: "INSERT INTO a_user(username, password_hash) VALUES ($1, $2)",
+      values: [username , passwordHash]
+    }
+    await pool.query(query);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// function checkUser(username, password) {
-//   const query = {
-//     text: "SELECT password_hash FROM a_user WHERE username = $1",
-//     values: [username]
-//   }
-//   pool.query(query, (err, res) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(res.fields);
-//     console.log(res.rows)
-//     bcrypt.compare(password, res.rows[0].password_hash, (err, result) => {
-//       if (err) {
-//         console.log(err);
-//       }
-//       console.log(result);
-//     })
-//   })
-// }
+const checkUser = async (username, password) => {
+  let user = {};
+  try {
+    const query = {
+      text: "SELECT password_hash FROM a_user WHERE username = $1",
+      values: [username]
+    }
+    user = await pool.query(query);
+    if (user.rowCount === 0) return Promise.reject('Such user does not exist.') 
+    try {
+      
+      const result = await bcrypt.compare(password, user.rows[0].password_hash);
+      return result;
+    } catch(bcryptError) {
+      console.log('Bcrypt compare error: ' + bcryptError);
+    }
+    console.log(user);
+  } catch (queryError) {
+    console.log('Query error: ' + queryError);
+  }
+
+
+}
+
 
 /* ENDOF DB and BCRYPT */
 
@@ -59,36 +58,44 @@ function generateAccessToken(username) {
   return jwt.sign({ username }, TOKEN_SECRET, { expiresIn: "1h"})
 }
 
-function authenticateToken(req, res, next) {
-  // Gather the jwt access token from the request header
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  console.log(token);
-  if (token == null) return res.sendStatus(401) // if there isn't any token
-
-  jwt.verify(token, TOKEN_SECRET, (err, user) => {
-    console.log(err)
-
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next() // pass the execution off to whatever request the client intended
-  })
-}
-
 // Middlewares
 app.use(cors())
 app.use(express.json())
 // app.use(authRoutes)
 
 // Routes
-
-app.post('/login', (req, res) => {
+app.post('/signup', async (req, res) => {
   const { username, password } = req.body;
-  let response = {token: null};
-  if (username === 'admin' && password === 'admin') {
-    response = {token: 'sometoken'}
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log(hashedPassword);
+    await storeUser(username, hashedPassword)
+  } catch(error) {
+    console.log(error);
   }
-  res.send(response)
+
+  res.send({})
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const token = generateAccessToken(username)
+
+  let response = {};
+
+  try {
+    const userHasValidCredentials = await checkUser(username, password);
+    if (userHasValidCredentials) {
+      response = {token,}
+    }
+    res.send(response)
+  } catch(error) {
+    console.log('Credentials are incorrect: ' + error);
+    res.send({error})
+  }
+
+
 })
 
 
